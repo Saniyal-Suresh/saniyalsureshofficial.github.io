8a63f979f861b3273aecc518b4c2a2e20c900601
saniyalsuresh2@gmail.com <saniyalsuresh2@gmail.com>
2023-07-28 00:54:44 +0530

    three updates

---
+++ BPSS.js
@@ -0,0 +1,28 @@
+//variables
+let btn = document.querySelector(".clc-btn");
+let amt = document.querySelector("#amount");
+let schemeMonths = document.querySelector("#totalMonths");
+let advEmi = document.querySelector("#advanceMonths");
+//dipalys
+let dbdDisplay = document.querySelector(".dbdDisplay");
+let emiDisplay = document.querySelector(".emiDisplay");
+let chargesDisplay = document.querySelector(".chargesDisplay");
+let ipDisplay = document.querySelector(".ipDisplay");
+//const values
+const dbd = 1.77;
+const emiCard = 412;
+const addonCard = 199;
+const instaCard = 118;
+
+btn.addEventListener("click", () => {
+    let proCharge = Number(prompt("Enter product charge"))
+  let dbdCalc = Math.round((amt.value * 1.77) / 100) + 1;
+  let emiCalc = Math.round((amt.value / schemeMonths.value))+1;
+  let charges = Number(dbdCalc+emiCard+addonCard+instaCard);
+  let sumCharge = charges+proCharge
+  let emiMonths = schemeMonths.value - advEmi.value;
+  dbdDisplay.innerHTML = "DBD : " + dbdCalc;
+  emiDisplay.innerHTML = "EMI : " + emiCalc + " Rs. "+emiMonths+" months";
+  chargesDisplay.innerHTML = "Charges : "+sumCharge;
+  ipDisplay.innerHTML = "IP / Down Payment = "+ (sumCharge+emiCalc)
+});
